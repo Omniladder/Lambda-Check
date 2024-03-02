641227614d259f3997c5f3df9b2f483aa98bdb25
idugan100 <isaacd4444@gmail.com>
2024-03-02 17:21:23 -0500

    moving logic into functions

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,26 @@ module Main where
 import Data.List.Split
 import Data.List
 
+removeComments :: [String] -> [String]
+removeComments input = filter (\x -> not (isInfixOf "--" x)) input
+
+splitOnNewLine :: String -> [String]
+splitOnNewLine input = splitOn "\n" input
+
+addWithNewLines :: [String] -> [String]
+addWithNewLines input = map (++ "\n") input
 
 main :: IO ()
 main = do
     fileContent <- readFile "Lambda-Check.cabal"
-    let split_output = splitOn "\n" fileContent
-    let no_comments = filter (\x -> not (isInfixOf "--" x)) split_output
-    let filtered_output = dropWhile ( \x -> not (isInfixOf "build-depends:" x)) no_comments
-    print filtered_output
 
+    let split_output = splitOnNewLine fileContent
+
+    let no_comments = removeComments split_output
+
+    let add_new_lines = addWithNewLines no_comments
+
+    let new_string = concat add_new_lines
+
+    putStrLn new_string
     
